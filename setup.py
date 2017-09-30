db9d79c29cf9b795f43523c7ee99d3ee8800ab94
Alex Grönholm <alex.gronholm@nextday.fi>
2017-10-01 02:40:53 +0300

    Updated packaging/testing/documentation configuration

--- setup.py
+++ setup.py
@@ -1,59 +1,12 @@
-from pathlib import Path
-
 from setuptools import setup
 
 setup(
-    name='asphalt-mailer',
     use_scm_version={
         'version_scheme': 'post-release',
         'local_scheme': 'dirty-tag'
     },
-    description='Mailer component for the Asphalt framework',
-    long_description=Path(__file__).with_name('README.rst').read_text('utf-8'),
-    author='Alex Grönholm',
-    author_email='alex.gronholm@nextday.fi',
-    url='https://github.com/asphalt-framework/asphalt-mailer',
-    classifiers=[
-        'Development Status :: 5 - Production/Stable',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: Apache Software License',
-        'Topic :: Communications :: Email',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6'
-    ],
-    license='Apache License 2.0',
-    zip_safe=False,
-    packages=[
-        'asphalt.mailer',
-        'asphalt.mailer.mailers'
-    ],
     setup_requires=[
+        'setuptools >= 36.2.7',
         'setuptools_scm >= 1.7.0'
-    ],
-    install_requires=[
-        'asphalt >= 3.0, < 5.0',
-        'aiosmtplib ~= 1.0',
-        'typeguard ~= 2.0'
-    ],
-    extras_require={
-        'testing': [
-            'aiosmtpd',
-            'pytest',
-            'pytest-cov',
-            'pytest-catchlog',
-            'pytest-asyncio >= 0.5.0'
-        ]
-    },
-    entry_points={
-        'asphalt.components': [
-            'mailer = asphalt.mailer.component:MailerComponent'
-        ],
-        'asphalt.mailer.mailers': [
-            'mock = asphalt.mailer.mailers.mock:MockMailer',
-            'smtp = asphalt.mailer.mailers.smtp:SMTPMailer',
-            'sendmail = asphalt.mailer.mailers.sendmail:SendmailMailer'
-        ]
-    }
+    ]
 )
